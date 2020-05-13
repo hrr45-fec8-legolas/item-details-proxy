adf8a172fde4005bb7206500fafdb01db89b36f1
stephenJohnGiardina <stephengia@yahoo.com>
2020-05-13 18:38:49 -0400

    Linted and removed redundant code

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/../public/')));
 
-app.get('/', (req, res) => {
-  res.end();
-})
-
 const singleItemProxy = createProxyMiddleware({target: 'http://localhost:3001'});
 const itemDetailsProxy = createProxyMiddleware({target: 'http://localhost:3002'});
 const relatedItemsProxy = createProxyMiddleware({target: 'http://localhost:3003'});
