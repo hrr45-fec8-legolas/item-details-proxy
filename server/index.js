bd27a9b96e63a6365a030fb350b933b7e6b6bb48
stephenJohnGiardina <stephengia@yahoo.com>
2020-05-13 13:28:43 -0400

    All modules rendering on page with data

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/../public/')));
+
+app.get('/', (req, res) => {
+  res.end();
+})
+
+const singleItemProxy = createProxyMiddleware({target: 'http://localhost:3001'});
+const itemDetailsProxy = createProxyMiddleware({target: 'http://localhost:3002'});
+const relatedItemsProxy = createProxyMiddleware({target: 'http://localhost:3003'});
+const reviewsProxy = createProxyMiddleware({target: 'http://localhost:3004'});
+
+app.use('/api/items/:id', singleItemProxy);
+app.use('/items/:id', itemDetailsProxy);
+app.use('/api/related_products/:id', relatedItemsProxy);
+app.use('/api/allreviews/', reviewsProxy);
+
+const PORT = 3005;
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
